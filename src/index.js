f1234d538a348d7fe92d2a6e91c9388c93807a07
luis-jr-zembruski <luis.jr.zembruski@icloud.com>
2020-10-27 11:22:41 -0300

    middleware

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+const { request } = require('express');
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -7,6 +8,31 @@ app.use(express.json());
 
 // Array temporário para guardar as informações
 const projects = [];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.time(logLabel);
+
+  next(); // Próximo middleware
+
+  console.timeEnd(logLabel);
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if(!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid project ID.'});
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId);
 
 app.get('/projects', (request, response) => {
 
